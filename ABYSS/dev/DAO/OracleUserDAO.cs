fcd13e6ddcf23a1f853d8badde9dc9c00a473feb
Spaeda <Spaeda@be50a92c-002a-0410-a432-e70bfe6ed8b7>
2007-04-16 18:22:55 +0000

    Add Oracle function. problems : Disparition de la calsse oracleUser dans buisness ?

--- ABYSS/dev/DAO/OracleUserDAO.cs
+++ ABYSS/dev/DAO/OracleUserDAO.cs
@@ -12,28 +12,82 @@ namespace DAO {
         #region Public Static Methods
         public static void SaveOracleUser(OracleUserData oracleUserData) {
             string query = "CREATE USER " + oracleUserData.UserLogin +
-                    "IDENTIFIED BY " + oracleUserData.UserPassword +
-                    "DEFAULT TABLESPACE " + oracleUserData.DefaultTablespace +
-                    "TEMPORARY TABLESPACE " + oracleUserData.TemporatyTablespace +
-                    "PROFILE " + oracleUserData.Profile;
+                    " IDENTIFIED BY " + oracleUserData.UserPassword +
+                    " DEFAULT TABLESPACE " + oracleUserData.DefaultTablespace +
+                    " TEMPORARY TABLESPACE " + oracleUserData.TemporatyTablespace +
+                    " PROFILE " + oracleUserData.Profile;
 
             if (oracleUserData.PasswordExpire) {
-                query += "PASSWORD EXPIRE";
+                query += " PASSWORD EXPIRE";
             }
 
             if (oracleUserData.Account) {
-                query += "ACCOUNT UNLOCK";
+                query += " ACCOUNT UNLOCK";
             }
             else {
-                query += "ACCOUNT LOCK";
+                query += " ACCOUNT LOCK";
             }
 
-            OracleCommand cmd = getInstance().CreateCommand();            
+            ExecuteNonQuery(query);
+
+        }
+
+        public static void EditPasswordOracleUser(OracleUserData oracleUserData, string newPassword) {
+            string query = "ALTER USER " + oracleUserData.UserLogin + " IDENTIFIED BY " + newPassword + " REPLACE " + oracleUserData.UserPassword;
+        }
+
+        public static void LockOracleUser(OracleUserData oracleUserData) {
+            string query = "ALTER USER " + oracleUserData.UserLogin + " ACCOUNT LOCK";
+            ExecuteNonQuery(query);
+        }
+
+        public static void UnlockOracleUser(OracleUserData oracleUserData) {
+            string query = "ALTER USER " + oracleUserData.UserLogin + " ACCOUNT UNLOCK";
+            ExecuteNonQuery(query);
+        }
+
+        public static void EditTablespaces(OracleUserData oracleUserData) {
+            string query = "ALTER USER " + oracleUserData.UserLogin + " DEFAULT TABLESPACE " + oracleUserData.DefaultTablespace +
+                " TEMPORARY TABLESPACE " + oracleUserData.TemporatyTablespace;
+            ExecuteNonQuery(query);
+        }
+
+        public static void DeleteOracleUser(OracleUserData oracleUserData) {
+            string query = "DROP USER " + oracleUserData.UserLogin + " CASCADE";
+            ExecuteNonQuery(query);
+        }
+
+        public static OracleDataReader GetOracleUser() {
+            string query = "select USERNAME," +
+                   "USER_ID," +
+                   "DEFAULT_TABLESPACE," +
+                   "TEMPORARY_TABLESPACE," +
+                   "PASSWORD," +
+                   "ACCOUNT_STATUS," +
+                   "PROFILE," +
+                   "CREATED " +
+            "from DBA_USERS " +
+            "order by USERNAME";
+
+            return ExecuteReader(query);
+        }
+
+        public static void ExecuteNonQuery(string query) {
+            OracleCommand cmd = getInstance().CreateCommand();
             cmd.CommandType = CommandType.Text;
             cmd.CommandText = query;
             cmd.ExecuteNonQuery();
             cmd.Dispose();
+        }
 
+        public static OracleDataReader ExecuteReader(string query) {
+            OracleCommand cmd = getInstance().CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = query;
+            OracleDataReader reader = cmd.ExecuteReader();
+            cmd.Dispose();
+
+            return reader;
         }
         #endregion
     }
